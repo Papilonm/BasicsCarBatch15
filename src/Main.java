fa70e67eaefd6276322d48b2af88119b2cf33359
Papilonm <josephhl96p@gmail.com>
2023-03-30 20:02:25 -0400

    Lights have been added

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,15 @@ public class Main {
         audi.power = "5000cc";
 
 
-        Engine X=new Engine();
-        X.cylinders="8";
-        X.size="5200cc";
-        X.weight="7574kg";
-        
+        Engine X = new Engine();
+        X.cylinders = "8";
+        X.size = "5200cc";
+        X.weight = "7574kg";
 
+        Light light = new Light();
+        light.color = "White";
+        light.size = "15cm";
+        light.intensity = "500lumin";
 
     }
 }
